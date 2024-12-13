f2a65f088a0fc4e11a1432c6f5ca29da4e551836
“NikitaRai” <nikirai48@gmail.com>
2024-12-13 11:57:46 +0530

    New push

--- cypress/e2e/Features/Homepage.feature
+++ cypress/e2e/Features/Homepage.feature
@@ -1,9 +1,8 @@
 Feature: User login
 
+Background :I am on the login page And I login with valid credentials
+
   Scenario: User logs in with valid credentials
-    Given I visit the login page
-    When I enter valid credentials
-    Then I land on the dashboard
     Then  I click response desk tab
     Then I should land on Conversation sub tab
     Then I click on all the sub tabs
--- cypress/e2e/Stepdefinitions/loginsteps.cy.js
+++ cypress/e2e/Stepdefinitions/loginsteps.cy.js
@@ -2,25 +2,6 @@ import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 import LoginPage from "../PageObject/loginpage.cy";
 const loginPage = new LoginPage();
 
-Given("I visit the login page", () => {
-  loginPage.visit();
-});
-
-When("I enter valid credentials", () => {
-  const username = "nikirai48@gmail.com";
-  const password = "Humansofindia@11";
-  loginPage.enterUsername(username);
-  loginPage.enterPassword(password);
-  loginPage.submitLogin();
-});
-
-Then("I land on the dashboard", () => {
-  cy.url().should(
-    "include",
-    "https://prgloo-dev-admin-01.azurewebsites.net/#!/",
-  );
-});
-
 Then("I click response desk tab", () => {
   cy.get("#module-responseDesk > .v4icon").click();
 });
