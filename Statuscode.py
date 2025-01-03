16e7001ae418cee598937ca3b20be139e8841fe4
Nikita Rai <nikirai48@gmail.com>
2025-01-03 12:25:04 +0530

    Update Statuscode.py
    changing from remote to local

--- Statuscode.py
+++ Statuscode.py
@@ -9,7 +9,7 @@ def check_uptime():
         if response.status_code == 200:
             print(f"Application is UP! Status code: {response.status_code}")
         else:
-            print(f"Application is DOWN! Status code: {response.status_code}")
+            print(f"Application is not down beware! Status code: {response.status_code}")
     except requests.exceptions.RequestException as e:
         print(f"Application is DOWN! Error: {e}")
 
